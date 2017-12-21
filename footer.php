89ba01d979c9af7daa9717dac4f6f6a8ba862308
aritzcm <aritz.cintora@gmail.com>
2017-12-21 10:42:22 +0100

    Creado footer site

---
+++ footer.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+    <link rel="stylesheet" href="./css/style_footer.css" media="screen">
+    <meta charset="UTF-8">
+
+        </head>
+<footer>
+    <hr>
+<div>
+    <ul class="footer_top">
+        <li class="footer_nav"><a href="#" class="link_footer">Contacto</a></li>
+        <li class="footer_nav"><a href="#" class="link_footer">Política de privacidad</a></li>
+        <li class="footer_nav"><a href="#" class="link_footer">Política de coockies</a></li>
+    </ul>
+    </div> 
+    <div>
+        <p class="footer_info">Todos los vídeos y comentarios en este sitio son propiedad de sus respectivos usuarios autores.</p>
+        <p class="footer_info">Para ejercer sus derechos de copyright no dude en <a href="#" class="link_footer">contactar con nosotros.</a></p>
+        <p class="footer_info">BananaTube y sus administradores no se hacen responsables de las opiniones publicadas por los usuarios y colaboradores en el sitio.</p>
+    </div>
+    <br/>
+    
+</footer>
+</html>
