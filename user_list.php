377b7dd3ab8df61a405ea70bb7b33eff5e494d10
aritzcm <aritz.cintora@gmail.com>
2017-12-21 15:20:17 +0100

    Creación componente: "userlist"

---
+++ user_list.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <link rel="stylesheet" href="./css/style_user_list.css" media="screen">
+    <meta charset="UTF-8">
+
+        </head>
+<body id="userlist">
+<!-- Sección de imágenes -->    
+    <div class="userlist_image userlist">
+        <img src="img/teslaAvatar.png" height="100px">
+    </div> 
+<!-- sección de nombre/usuario-->    
+   <section class="userlist">
+        <div class="userlist_name userlist">
+        <p class="user_nombre">Nikola Tesla</p>
+        <p class="user_user">@PalomitaKawaii</p>
+    </div>
+     </section>
+<!-- Descripción -->    
+    <div class="userlist_lore userlist">
+        Fui un inventor, ingeniero mecánico, eléctrico y físico de origen serbio. Fan de la electricidad y las palomas. dghuidsghuisdrnhgiodehgiouaethaentbnduigeigheuirghuiernhguidbiugdfuibvuidfbgiuerbgiuaebriabhiub
+    </div>
+<!-- info de registro -->
+    <div class="userlist_when userlist">
+    </div>
+    
+</body>
+</html>
