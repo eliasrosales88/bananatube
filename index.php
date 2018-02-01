cd915c105ec1fd9133737c242eeb0815b54d6956
elias <eliasrosales88@gmail.com>
2018-02-01 23:28:50 +0100

    Creación del sidebar y posicionamiento absoluto con respectivos estilos en css. Los archivos que se crearon fueron sidebar.php en la carpeta components, style_sidebar.css en la carpeta css con la vinculación en el archivo principal styles.css

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
   <title>Document</title>
 </head>
 <body class="container-fluid bg_main" >
+  <div id="sidenav" class="row z-menu">
+    <?php include "./components/sidebar.php";?>
+  </div>
   <?php include "./components/header.php";?>
   <?php include "./components/footer.php";?>
   </body>
