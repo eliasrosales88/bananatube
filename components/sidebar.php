b03cb135333c18db2a7cc3b23136478f866f80c8
Elias Rosales <eliasrosales88@gmail.com>
2018-02-02 13:24:33 +0100

    Se incluyen los enlaces en el sidebar con sus iconos. Los enlaces están vacíos, falta asignarlos

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,10 +1,23 @@
 <div id="sidenav" class="row z-menu" hidden >
     <aside id="sidebar" class="col-xl-3 col-lg-3 col-md-4 col-sm-6 col-8 bg_aside">
         <div class="row bg_aside_top">
-            <h1>MENU TOP</h1>
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center">
+                <img src="..." alt="..." class="rounded">
+            </div>
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center text-white">
+                <span>orangutan@banana.com</span>
+            </div>
         </div>
         <div class="row ">
-            <h1>LISTA ITEMS</h1>
+            <ul class="list-group border-0">
+                <li class="list-group-item border-0"><i class="fa fa-home mr-4"></i><a href="">Inicio</a></li>
+                <li class="list-group-item border-0"><i class="fa fa-address-card mr-4"></i><a href="">Perfil</a></li>
+                <li class="list-group-item border-0"><i class="fa fa-cloud-upload mr-4"></i><a href="">Subir vídeos</a></li>
+                <li class="list-group-item border-0"><i class="fa fa-image mr-4"></i><a href="">Gestión de contendidos</a></li>
+                <li class="list-group-item border-0"><i class="fa fa-users mr-4"></i><a href="">Amigos</a></li>
+                <li class="list-group-item border-0"><i class="fa fa-cog mr-4"></i><a href="">Configuración</a></li>
+                <li class="list-group-item border-0"><i class="fa fa-pencil mr-4"></i><a href="">Enviar sugerencias</a></li>
+            </ul>
         </div>
     </aside>
     <div id="dark" class="col-xl-9 col-lg-9 col-md-8 col-sm-6 col-4 bg_dark">
