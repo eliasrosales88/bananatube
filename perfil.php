ac13c27700f8414427b027f446f8729e371cd4ad
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-02-22 13:37:29 +0100

    nueva perfil.php

--- perfil.php
+++ perfil.php
@@ -25,27 +25,12 @@
         <div class="col text-center">
             @PalomitaKawaii
         </div>
-        <div class="col text-center">
+        <div class="col text-center mt-2">
             Fui un inventor, ingeniero mecánico, eléctrico y físico de origen serbio. Fan de la electricidad y las palomas.
         </div>
-        <div class="row">
-            <div class="col-1 col-sm-2 col-md-3 col-lg-4 col-xl-4"></div>
-            <div class="table-responsive-md col-10 col-sm-8 col-md-6 col-lg-4 col-xl-4">
-                <table class="table green">
-                    <tbody>
-                        <tr>
-                            <td>Vídeos subidos</td>
-                            <td>Todos mis vídeos</td>
-                            <td>Compartidos</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <div class="col-1 col-sm-2 col-md-3 col-lg-4 col-xl-4"></div>
-        </div>
-        <div class="row">
-            <div class="col-4"></div>
-            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <div class="row mt-4">
+            <div class="col-1 col-sm-2 col-md-3 col-lg-4"></div>
+            <ul class="nav  mb-3 col-10 col-sm-8 col-md-6 col-lg-4" id="pills-tab" role="tablist">
                 <li class="nav-item">
                     <a class="nav-link green" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Vídeos subidos</a>
                 </li>
@@ -56,17 +41,38 @@
                     <a class="nav-link green" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">Compartidos</a>
                 </li>
             </ul>
-            <div class="col-4"></div>
+            <div class="col-1 col-sm-2 col-md-3 col-lg-4"></div>
         </div>
 
         <div class="row">
-            <div class="col-4"></div>
-            <div class="tab-content col-4" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">TEST 1</div>
-                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">TEST2</div>
-                <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">TEST 3</div>
+            <div class="col-1 col-sm-2 col-md-3 col-lg-4"></div>
+            <div class="tab-content col-10 col-sm-8 col-md-6 col-lg-4" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                    <p>Video 1</p>
+                    <p>
+                        <video width="320" height="240" controls>
+                            <source src="./videos/bunny.mp4" type="video/mp4">
+                        </video>
+                    </p>
+                </div>
+                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                    <p>Video 2</p>
+                    <p>
+                        <video width="320" height="240" controls>
+                            <source src="./videos/bunny.mp4" type="video/mp4">
+                        </video>
+                    </p>
+                </div>
+                <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                    <p>Video 3</p>
+                    <p>
+                        <video width="320" height="240" controls>
+                            <source src="./videos/bunny.mp4" type="video/mp4">
+                        </video>
+                    </p>
+                </div>
             </div>
-            <div class="col-4"></div>
+            <div class="col-1 col-sm-2 col-md-3 col-lg-4"></div>
         </div>
         <!-- END del container fluid -->
     </div>
