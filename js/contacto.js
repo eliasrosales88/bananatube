dd27660930dd109fbf84950db4756e9b6869ed55
eduardomignotescalada <eduardo.mignot@gmail.com>
2018-03-07 17:09:31 +0100

    creacion de nuevo fichero js

---
+++ js/contacto.js
@@ -0,0 +1,35 @@
+function peticionPost() {
+    console.log("Click pulsado!");
+    /*var objeto = {
+    };
+    objeto.textarea=$("#comments").val();*/
+    var textarea = $("#comments").val();
+
+    console.log(textarea);
+
+    /*$.post("#url",textarea,function(datos){
+    console.log("success");
+    $("#resultados").html(datos);
+    }
+    .done(function () {
+    console.log("second success");
+    })
+    .fail(function () {
+    console.log("error");
+    })
+    .always(function () {
+    console.log("finished");
+    });
+     
+    })*/
+
+}
+
+
+
+function init() {
+    console.log("DOM Cargado Jquery");
+    $("#boton").click(peticionPost);
+}
+
+$(document).ready(init);
